d22fc76a899decbb6a4f21d7e44147b0df593b6f
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-10-14 14:12:14 -0800

    homepage displays placeholder champions

---
+++ src/components/organisms/DisplayChampions/index.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState, useEffect } from 'react'
+const axios = require("axios")
+
+// import { Link } from 'react-router-dom'
+//Call data.keys() on https://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json to grab list of champions.
+function DisplayChampions() {
+    const [champions1, setChampions1] = useState([])
+    useEffect( () => {
+        async function postrequest() {
+            const champions = ["Tryndamere", "Jax", "Khazix"]
+            const p = champions.map((champ) => {
+                return axios({
+                    url: 'http://localhost:5000/graphql',
+                    method: 'post',
+                    data: {
+                        query: `
+                        query Champion {
+                            champion(name: "${champ}") {
+                            name
+                            image {
+                                full
+                            }
+                            }
+                            }
+                        `
+                    }
+                })
+            })
+            // console.log('here')
+            // console.log(p)
+            const values = await axios.all(p)
+            // console.log(values)
+            const JSX_champ = values.map((champion) => {
+                // console.log(`Champion Variable:`)
+                // console.log(champion.data.data.champion)
+                const { name, image } = champion.data.data.champion
+                return (
+                    <li>
+                        <img src={`http://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/${image.full}`}/>
+                        <h1>{name}</h1>
+                    </li>
+                )
+            })
+            setChampions1(JSX_champ)
+        }
+        postrequest()
+    }) 
+    
+    return (
+        <div>
+            {champions1}
+        </div>
+    )
+}
+export default DisplayChampions
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/organisms/Navbar'
 import HomeLogo from '../components/organisms/HomeLogo'
+import DisplayChampions from '../components/organisms/DisplayChampions'
+const axios = require("axios")
+
 function Home() {
+    // useEffect( async () => {
+    //     const champions = ["Tryndamere", "Jax"]
+    //     const requests = []
+    //     async function postrequest(champion) {
+    //         const data = await axios({
+    //             url: 'http://localhost:5000/graphql',
+    //             method: 'post',
+    //             data: {
+    //               query: `
+    //                 query Champion {
+    //                   champion(name: "${champion}") {
+    //                     id
+    //                     name
+    //                     image {
+    //                         full
+    //                     }
+    //                     }
+    //                   }
+    //                 `
+    //             }
+    //           })
+    //         console.log(data.data)
+    //     }
+    //     champions.forEach(async champion => {
+    //         console.log(champion)
+    //         await postrequest(champion)
+    //     });
+    //     // const data2 = await axios.all(requests)
+    //     // console.log(data2)
+    //     // postrequest()
+    // })
+    
+    
     return (
         <div>
             <Navbar/>
             <HomeLogo/>
-            <h1>Home</h1>
-            <h3>Choose Your<text> Legend</text></h3>
-            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."/>            
-            {/* Map a list of every champion 
-            return a component that contains name, and icon  */}
+            <h3>Choose Your Legend</h3>
+            <input type="text" id="myInput"  placeholder="Search for names.."/>      
+            <DisplayChampions/>
         </div>
     )
 }
