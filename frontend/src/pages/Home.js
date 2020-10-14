eb21823eba0012496cf67af63bd3a2c9004d29cc
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-10-14 15:35:05 -0800

    made frontend folder

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import Navbar from '../components/organisms/Navbar'
+import HomeLogo from '../components/organisms/HomeLogo'
+import DisplayChampions from '../components/organisms/DisplayChampions'
+import DisplayChampion from '../components/organisms/DisplayChampion'
+const axios = require("axios")
+
+function Home() {
+    
+    return (
+        <div>
+            <Navbar/>
+            <HomeLogo/>
+            <main>
+                <section className="home">
+                    <div className="champions">
+                        <div className="champion-text">
+            <h1>Home</h1>
+            <h3>Choose Your<text> Legend</text></h3>
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."/>            
+            {/* Map a list of every champion 
+            return a component that contains name, and icon  */}
+            <DisplayChampions/>
+        </div>
+        </div>
+        </section>
+        </main>
+        </div>
+    )
+}
+
+export default Home
