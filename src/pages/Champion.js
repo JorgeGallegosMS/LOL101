c1453452b92a9e6dcfff2c482a8f44eba0cb7da9
HolyPi <joanelly2018@gmail.com>
2020-10-14 18:40:32 -0400

    Test to push

--- src/pages/Champion.js
+++ src/pages/Champion.js
@@ -5,7 +5,7 @@ function Champion() {
     return (
         <div>
             <Navbar/>
-            <h1>Champion!</h1>
+            <h1>Champion</h1>
             {/* Create grid:
             Display all champion info (Needs info first) */}
         </div>
