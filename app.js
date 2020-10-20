9b10e7f768a55e09e456114f909fdd0d4136d995
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-10-20 01:06:25 -0500

    heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/graphql', graphqlHTTP({
 
 app.use(express.static('frontend/build'))
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'))
 })
 
 app.listen(port, () => console.log(`Server started at http://localhost:${port}/graphql`))
---
+++ frontend/build/precache-manifest.2e75a34b66479c5affdd7acabada514f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc52c1358e900761ef278ebcb5c4c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8f05ed6fb068d693f0",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "24c38ab98bd2ecba91f8",
+    "url": "/static/css/main.9392532c.chunk.css"
+  },
+  {
+    "revision": "9b8f05ed6fb068d693f0",
+    "url": "/static/js/2.98511fe1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.98511fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c38ab98bd2ecba91f8",
+    "url": "/static/js/main.23c04115.chunk.js"
+  },
+  {
+    "revision": "2fa530c6b5d6b8dbe9a2",
+    "url": "/static/js/runtime-main.a357f2a1.js"
+  },
+  {
+    "revision": "967eefbeb708cbd9dbb76f6bb97bd17a",
+    "url": "/static/media/ahri.967eefbe.png"
+  },
+  {
+    "revision": "882cf7c6481f4ec3e0774f6bba529a32",
+    "url": "/static/media/logo1.882cf7c6.png"
+  },
+  {
+    "revision": "a039b2991555c341d4242e84dd7fbf10",
+    "url": "/static/media/test1-edit.a039b299.jpg"
+  },
+  {
+    "revision": "f6e607968192fc4623e11838501854ba",
+    "url": "/static/media/test2-edit.f6e60796.jpg"
+  }
+]);
